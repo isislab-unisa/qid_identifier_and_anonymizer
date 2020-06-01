d7e2ecd5c1704930af9ea67411075cbb0ec47feb
MatteoPastore <43520563+MatteoPastore@users.noreply.github.com>
2020-06-01 17:44:23 +0200

    Update

--- src/MatchingCsv.py
+++ src/MatchingCsv.py
@@ -9,7 +9,7 @@ else:
 	file_name = os.path.basename(file_path).split(".")[0]
 a = pd.read_csv("C:\\Users\\matte\\Desktop\\Framework\\dataset_newIndex.csv")
 b = pd.read_csv(file_path)
-#b = pd.read_csv("C:\\Users\\matte\\Desktop\\TesiGennaio\\quasi_identifier_hunter-master\\patenti__dataset\\ValleAosta\\GenereSingletonValleAosta.csv")
+
 merged = a.merge(b, on='newId')
 merged.to_csv("merged.csv", index=False)
 
@@ -21,7 +21,7 @@ while i<len(merged):
 	if i%10000==0:
 		print(i)
 	if merged.iloc[i]['anno_nascita_x']!= merged.iloc[i]["anno_nascita_y"] or merged.iloc[i]['comune_residenza_x']!= merged.iloc[i]["comune_residenza_y"]  or  merged.iloc[i]['sesso_x']!= merged.iloc[i]["sesso_y"]:
-		#print(merged.iloc[i])
+		
 		modifiedLines=modifiedLines+1
 	if merged.iloc[i]['anno_nascita_x'] and pd.isnull(merged.iloc[i]["anno_nascita_y"]) or merged.iloc[i]['comune_residenza_x'] and pd.isnull(merged.iloc[i]["comune_residenza_y"]) or merged.iloc[i]['sesso_x'] and pd.isnull(merged.iloc[i]["sesso_y"]):
 		deletedLines=deletedLines+1
