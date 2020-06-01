ae0f8ee4902110540cef40f50811790d7ee27408
MatteoPastore <43520563+MatteoPastore@users.noreply.github.com>
2020-06-01 17:43:41 +0200

    Update

--- src/IntervalKGroups.py
+++ src/IntervalKGroups.py
@@ -14,22 +14,15 @@ columns_names=["newId","anno_nascita", "comune_residenza", "sesso"]
 columnIndex=data["newId"]
 newData=pd.DataFrame(columns= columns_names,index=columnIndex)
 newData=data.sort_values("anno_nascita")
-#print(newData)
+
 newData.dropna(inplace=True)
 columnDate=newData["anno_nascita"].to_numpy()
 newColumnDate = list(set(columnDate))
-#print(newColumnDate)
-#print (columnDate)
-#print(len(data))
+
 n=4
 arrayNewCentroids=[]
-'''newColumnDate=list(zip(*[iter(columnDate)]*k))
-print(newColumnDate)
-print(len(newColumnDate))
-print(len(columnDate))
-'''
 newColumnDate = [newColumnDate[i * n:(i + 1) * n] for i in range((len(newColumnDate) + n - 1) // n )]  
-#print ( "Gruppi da ", n , newColumnDate) 
+ 
 
 for array in newColumnDate:
 	middle = float(len(array))/2
@@ -37,12 +30,11 @@ for array in newColumnDate:
 			arrayNewCentroids.append(array[int(middle - .5)])
 	else:
 			arrayNewCentroids.append((array[int(middle)]))
-#print(" Centroidi : ", arrayNewCentroids)
-#NewColumn=columnDate.astype(int)
+
 NewColumn=columnDate.tolist()
 NewColumn = [ int(x) for x in NewColumn ]
 
-#print(type(newColumnDate[1][0]))
+
 i=0
 d=0
 while i < len(columnDate):
@@ -55,28 +47,7 @@ while i < len(columnDate):
 		d=d+1
 	i=i+1
 	d=0
-#print(NewColumn)
-#print(len(NewColumn))
-#print(len(columnDate))
 
-
-
-
-'''
-i=0
-count=-1
-for array in newColumnDate:
-	count=count+1
-	i=0
-	while i<len(array):
-		array[i]=int(arrayNewCentroids[count])
-		i=i+1
-print(newColumnDate)
-
-newColumnDate=np.concatenate(newColumnDate)
-print(newColumnDate)
-newColumnDate=newColumnDate.astype(int)
-'''
 newData["anno_nascita"]=NewColumn
 
 newData.to_csv(file_name+"Range.csv", index=False)
